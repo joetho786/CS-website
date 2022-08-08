4b530f3c744a129cd3effbe271dd0ee8f9366619
KishoreKumar278 <89623174+KishoreKumar278@users.noreply.github.com>
2022-08-09 00:14:00 +0530

    Update make_json_pg.py

--- cs/make_json_pg.py
+++ cs/make_json_pg.py
@@ -20,9 +20,9 @@ def make_json(csvFilePath, jsonFilePath):
             data['role'] = rows[2]
             data['email'] = rows[3]
             data['whatsapp'] = rows[4]
-            data['fb'] = rows[5]
-            data['instagram'] = rows[6]
-            data['linkedin'] = rows[7]
+            data['fb'] = rows[6]
+            data['instagram'] = rows[7]
+            data['linkedin'] = rows[8]
             data['image'] = 'team_2021_images/PG/' + rows[1] + '.jpg'
 
             ls.append(data)
@@ -37,8 +37,8 @@ def make_json(csvFilePath, jsonFilePath):
   
 # Decide the two file paths according to your  
 # computer system 
-csvFilePath = r'PG_Team_2021.csv'
-jsonFilePath = r'swc_pg_team_2021.json'
+csvFilePath = r'PG_Team_2022.csv'
+jsonFilePath = r'swc_pg_team_2022.json'
   
 # Call the make_json function 
 make_json(csvFilePath, jsonFilePath)
